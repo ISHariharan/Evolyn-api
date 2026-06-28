549a63bc49b5090edb26faf0d26368fc4efaf99e
ISHariharan <ishariharan2004@gmail.com>
2026-06-28 18:30:00 +0530

    Change: adding helper functions to DTO of bankDetails and cash details

--- src/main/java/com/evolyn/api/BankDetails/StoreDetails/BankDetailsDTO.java
+++ src/main/java/com/evolyn/api/BankDetails/StoreDetails/BankDetailsDTO.java
@@ -5,4 +5,29 @@ public class BankDetailsDTO {
     public String userId;
     public accountDetailsDTO accountDetails;
     public cashDetailsDTO cashDetails;
+
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public accountDetailsDTO getAccountDetails() {
+        return accountDetails;
+    }
+
+    public void setAccountDetails(accountDetailsDTO accountDetails) {
+        this.accountDetails = accountDetails;
+    }
+
+    public cashDetailsDTO getCashDetails() {
+        return cashDetails;
+    }
+
+    public void setCashDetails(cashDetailsDTO cashDetails) {
+        this.cashDetails = cashDetails;
+    }
 }
--- src/main/java/com/evolyn/api/BankDetails/StoreDetails/cashDetailsDTO.java
+++ src/main/java/com/evolyn/api/BankDetails/StoreDetails/cashDetailsDTO.java
@@ -2,4 +2,12 @@ package com.evolyn.api.BankDetails.StoreDetails;
 
 public class cashDetailsDTO {
     public String balanceAmount;
+
+    public String getBalanceAmount(String balanceAmount){
+        return balanceAmount;
+    }
+
+    public void setBalanceAmount(String balanceAmount){
+        this.balanceAmount = balanceAmount;
+    }
 }
